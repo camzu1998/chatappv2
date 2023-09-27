4217052b745e4deb2cb07ca04c79ee5e5766bce5
Kamil Langer <kamil.langer@caops.pl>
2023-09-27 23:04:08 +0200

    Bootstrap install

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,11 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import "bootstrap/scss/bootstrap";`
+            }
+        }
+    }
 });
